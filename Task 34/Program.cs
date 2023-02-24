d0df174c216b4d2c35eba5f548e2e9c17fe5e5a4
Raurut <makey@yandex.ru>
2023-02-24 12:20:32 +0300

    Task 34 complete

---
+++ Task 34/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+int[] array = randomArray(7);
+Console.WriteLine($"[{string.Join(",",array)}] -> {countOfEven(array)}");
+
+int[] randomArray(int size) {
+    Random random = new Random();
+    int[] array = new int[size];
+    for(int i = 0; i < array.Length; i++) {
+        array[i] = random.Next(100,1000);
+    }
+    return array;
+}
+
+int countOfEven(int[] array) {
+    int count = 0;
+    for(int i = 0; i < array.Length; i++) {
+        if(array[i] % 2 == 0) {
+            count++;
+        }
+    }
+    return count;
+}
